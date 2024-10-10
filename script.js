09cc68ba2e4b88901938f09a21611e6da6b27f50
Dado Watt <dadowatt92@gmail.com>
2024-10-10 21:56:14 +0000

    Mise à jour du code avec ajout de nouvelles classes CSS

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ function fetchProduct(){
         }return Response.json();
     })
     .then(data => {
-        console.log(data);
+        // console.log(data);
         let produit = data.products;
-        produit.forEach(product=>{
+        produit.forEach(product => {
             const elementProduit = document.querySelector(".product");
-            
+
             const produitItems = document.createElement("div");
-            produitItems.classList.add("col-3");
-        
+            produitItems.classList.add("col-12","col-sm-6","col-md-4", "col-lg-3");
+
             produitItems.innerHTML = `<div class="card" style="width: 18rem;">
-            <img src="${product.thumbnail}" class="card-img-top" alt="...">
+            <img src="${product.thumbnail}" class="card-img-top bg-light" style="height: 15rem;" alt="...">
             <div class="card-body">
               <h4 class="card-title">${product.title}</h4>
               <p class="card-subtitle">${product.description}</p>
-              <p class="card-text">${product.price}</p>
+              <p class="card-text fw-bold">$${product.price}</p>
             </div>
           </div>`;
             elementProduit.appendChild(produitItems)
@@ -30,4 +30,3 @@ function fetchProduct(){
 }
 
 fetchProduct()
-// titre, description, prix, image
